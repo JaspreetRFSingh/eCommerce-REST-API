3b1c65d0df19efe1e0f13eb8e44d1a747ac19096
Jaspreet Singh <jaspreetsingh_tuli@epam.com>
2020-08-06 17:33:37 +0530

    Basic console app to generate the token

---
+++ HPlusSport.Console/Program.cs
@@ -0,0 +1,28 @@
+﻿using IdentityModel.Client;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace HPlusSport.Console
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var client = new HttpClient();
+
+            var discoveryDocument = await client.GetDiscoveryDocumentAsync(
+                "http://localhost:61884");
+
+            var tokenResponse = await client.RequestClientCredentialsTokenAsync(
+                new ClientCredentialsTokenRequest
+                {
+                    Address = discoveryDocument.TokenEndpoint,
+                    ClientId = "client",
+                    ClientSecret = "H+ Sport",
+                    Scope = "hps-api"
+                });
+            System.Console.WriteLine($"Token: {tokenResponse.AccessToken}");
+        }
+    }
+}
